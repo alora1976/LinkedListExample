e877cfe8a99235730dbebd6d82d90c60f0a89788
alora1976 <39773598+alora1976@users.noreply.github.com>
2019-09-04 17:38:25 -0700

    Update LinkedListExample.py

--- LinkedListExample.py
+++ LinkedListExample.py
@@ -1,82 +1,84 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep  1 20:08:29 2019
-
-@author: Lori
-"""
-
-class Node :
-    def __init__(self , val= None ):
-        self.val=val
-        self.link=None
-class LinkedList :
-    def __init__( self ):
-        self.head=None
+class Node:
+    
+    def __init__(self, val=None):
+        self.val = val
+        self.link = None
+class LinkedList:
+    
+    def __init__ (self):
+        self.head = None
         
-    def insert_pos(self,pos,val ):
-         if pos > 0:
-             current_node=self.head
-             for count in range(pos -1):
-                 current_node=current_node.link
-             next_link=current_node.link
-             current_node.link=Node(val)
-             current_node.link.link=next_link
-         else:
-            next_link=self.head
-            self.head=Node(val)
-            self.head.link=next_link
+    def printlist(self):
+
+        current_node=self.head
+
+        while current_node !=None:
+
+            print(current_node.val)
+
+            current_node=current_node.link
+   
+        
+    def insert_pos(self,pos,val):
+        if pos> 0:
+            current_node = self.head
+            for count in range (pos -1):
+                current_node = current_node.link
+            next_link = current_node.link
+            current_node.link = Node (val)
+            current_node.link.link = next_link
+    def update_pos(self,pos,val):
+        current_node = self.head
+        for count in range (pos):
+            current_node.val = val
             
     def delete_pos(self,pos):
         if pos > 0:
-            current_node=self.head
-            for count in range(pos- 1):
-                current_node=current_node.link
-                current_node.link=current_node.link.link
-        else :
-            self.head=self.head.link
+            current_node = self.head
+            for count in range (pos - 1):
+                current_node = current_node.link
+            current_node.link = current_node.link.link
+        else:
+            self.head = self.head.link
             
-    def printlist(self):
-        current_node=self.head
-        while current_node !=None:
-            print(current_node.val)
-            current_node=current_node.link
-            
-    def update_pos(self,pos,val):
-        current_node=self.head
-        for count in range(pos):
-            current_node=current_node.link
-        current_node.val=val
-    def append(self,val ):
-        current_node=self.head
-        while current_node.link !=None:
-            current_node=current_node.link
-        current_node.link=Node(val)
-        
-my_list=LinkedList()
-my_list.head=Node(10)
+   
+n1=Node(10)#assign value inside node1
+n2=Node(20)#assign value inside node2
+n3=Node(30)#assign value inside node3
+n4=Node(40)
+n5=Node(50)
+print(n1.val)#prints value inside node
+print(n2.val)
+print(n3.val)
+print(n4.val)
+print(n5.val)
+print("\n")#creates a space between lines of code so this is easier to follow
 
-n2=Node(20)
+
+my_list=LinkedList()
+my_list.head =Node(10)
+n2 =Node(20)
 n3=Node(30)
 n4=Node(40)
 n5=Node(50)
-my_list.head.link=n2
+
+
+my_list.head.link =n2
 n2.link=n3
 n3.link=n4
 n4.link=n5
-my_list=LinkedList()
-my_list.head=Node(10)
-my_list.append(30)
-my_list.append(100)
+
+print("\n")#creates a space between lines of code so this is easier to follow
+
+n3.val=100
 my_list.printlist()
-my_list=LinkedList()
-my_list.head=Node(10)
+print("\n")#creates a space between lines of code so this is easier to follow
+
+#deleting from a linked list
 my_list.delete_pos(3)
 my_list.printlist()
-my_list=LinkedList()
-my_list.head=Node(5)
+print("\n")#creates a space between lines of code so this is easier to follow
 
-my_list.insert_pos(2,15)
-
+my_list.insert_pos(1,15)
 my_list.printlist()
-
-
+print("\n")#creates a space between lines of code so this is easier to follow    
